92f259738faae0cb376927ef5328def8de2c84c0
VitRehak <91064420+VitRehak@users.noreply.github.com>
2023-04-15 21:40:22 +0200

    Add files via upload

---
+++ RewardingSystem/src/test/java/cz/morosystem/RewardingSystem/RewardingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.morosystem.RewardingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
